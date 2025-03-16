faa04f3131e1e7bf03b17f3d2e1c1e986a580c82
Yuta Uchida <yuchida4dev@gmail.com>
2025-03-16 13:14:25 +0900

    feat: add Prettier configuration

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  printWidth: 100,
+  tabWidth: 2,
+  semi: true,
+};
